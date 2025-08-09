b7edccc1a34078e8e6f87a9f10ef976cd91daace
akashsamanta07 <as5740061@gmail.com>
2025-08-09 19:22:12 +0530

    Add txt

--- Facial_Recognition_Monitoring_System/settings.py
+++ Facial_Recognition_Monitoring_System/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2fa-cjihhd#yx&qakev3$k6hghc&cpi3)$e66#u&gy0%5($0r0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
